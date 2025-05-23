fef0967e2c3cf17b7764ddc69efe904b877af17c
prashantgharate <prashantgharate155@gmail.com>
2025-05-23 13:12:11 +0000

    Updated index.js and removed python files

--- mynodeapp/index.js
+++ mynodeapp/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
- 
-var app = express();//Respond with "hello world" for requests that hit our root "/"
+var app = express();
+
+// Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('Hi I am Prashant and you ');
-});//listen to port 3000 by default
+  res.send('Hi I am Prashant and you');
+});
+
+// Listen on port 3000 or environment-defined port
 app.listen(process.env.PORT || 3000);
- 
+
 module.exports = app;
+
