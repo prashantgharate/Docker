a9c97f0c61446e65351752f7fc327967b7d6fb89
prashantgharate <prashantgharate155@gmail.com>
2025-05-24 13:23:58 +0000

    three

---
+++ threetire/db/init.sql
@@ -0,0 +1,3 @@
+use facebook;
+create table users (id int not null primary key auto_increment, name varchar(100), email varchar(100), comment varchar(100), website varchar(100), gender varchar(100));
+
